25a31b3c4838543e217ea9e495309c443cf1a84a
Natalle <natallemoura7@gmail.com>
2019-08-30 15:04:14 +0200

    Total done

--- src/Student/Report.js
+++ src/Student/Report.js
@@ -50,31 +50,46 @@ export class ReportCard extends Component {
             );
         });
 
+        const totals = {
+            count: 0,
+            math: 0,
+            science: 0,
+            english: 0,
+            social: 0,
+            sum: 0
+        };
 
-
+        for (let student of students) {
+            totals.count += 1;
+            totals.math += student.marks.math;
+            totals.science += student.marks.science;
+            totals.english += student.marks.english;
+            totals.social += student.marks.social;
+            totals.sum += student.marks.math + student.marks.science + student.marks.english + student.marks.social;
+        }
 
         return (
             <table>
                 <tr>
-                    <th>id</th>
-                    <th>fullName</th>
-                    <th>marksMath</th>
-                    <th>marksScience</th>
-                    <th>marksEnglish</th>
-                    <th>marksSocial</th>
-                    <th>total</th>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Math</th>
+                    <th>Science</th>
+                    <th>English</th>
+                    <th>Social</th>
+                    <th>Sum</th>
                 </tr>
                 
                 { studentsHtml }
 
                 <tr>
-                    <th>total</th>
-                    <td>2</td>
-                    <td>8</td>
-                    <td>7</td>
-                    <td>8</td>
-                    <td>9</td>
-                    <td>32</td>
+                    <th>Total</th>
+                    <td>{totals.count}</td>
+                    <td>{totals.math / totals.count}</td>
+                    <td>{totals.science / totals.count}</td>
+                    <td>{totals.english / totals.count}</td>
+                    <td>{totals.social / totals.count}</td>
+                    <td>{totals.sum / totals.count}</td>
                 </tr>
             </table>
         );
